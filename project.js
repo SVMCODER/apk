6c6f6ccb9c2e3c45a8d35e4a9fb5c5252c5e7078
svmcoder <124701282+SVMCODER@users.noreply.github.com>
2023-05-29 12:28:38 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,31 @@
+document.getElementById('project').innerHTML = localStorage.getItem('project').replace('null', ``)
+function logout() {
+    swal({
+          title: "Are you sure?",
+          text: "You will be logged out and all your projects will be deleted!",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonColor: '#DD6B55',
+          confirmButtonText: 'Logout',
+          cancelButtonText: "Cancel",
+          closeOnConfirm: false,
+          closeOnCancel: false
+      },
+      function(isConfirm){
+      if (isConfirm){
+        localStorage.removeItem('loggedin')
+        localStorage.removeItem('name')
+        localStorage.removeItem('project')
+        localStorage.removeItem('badge')
+        localStorage.removeItem('role')
+        localStorage.removeItem('verified')
+        localStorage.removeItem('method')
+        swal("Logging Out", "Please wait", "success");
+        setTimeout(() => {
+          window.location.replace('/')
+        }, 1400);
+      } else {
+        swal("Cancelled", "You projects are safe!", "error");
+      }
+      });
+  }
